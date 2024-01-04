4a1eb7729d5ae5e983c401850e350b35c5f7ea48
Hanysisko <tymek@radlowski.net>
2024-01-04 16:09:58 +0100

    project init, first divs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
+import mebleImage from "./img/meble1.jpg";
+import stolikImage from "./img/Stolik.png";
+import zegarImage from "./img/Zegar.png";
+import mapaImage from "./img/mapagoogle.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
+      <div className="Menu">
+        <h1>Antyki Renowacja Mebli Zabytkowych</h1>
+        <p>strona główna</p>
+        <p>galeria</p>
+        <p>cennik</p>
+        <p>kontakt</p>
+      </div>
+
+      <div className="HeaderMain">
+        <div className="ImageMeble">
+          <img src={mebleImage} alt="ImageMeble1"></img>
+        </div>
+        <div className="HeaderText">
+          <h1>NASZĄ PASJĄ JEST ODNAWIANIE ANTYKÓW</h1>
+          <p>Myślisz o renowacji drzwi? Chcesz odświeżyć zabytkową szafę? Albo chcesz odrestaurować stary zegar?</p>
+          <div className="Button1">Zobacz nasze renowacje</div>
+        </div>
+      </div>
+
+      <div className="MiddleMain">
+
+      </div>
+
+      <div className="Gallery">
+
+      </div>
+
+      <div className="Pricing">
+        <h1>Cennik</h1>
+        <p>Każdą cenę renowacji ustalamy indywidualnie biorąc pod uwagę stan zniszczeń, sumę nakładu pracy oraz materiałów potrzebnych do wykonania danej renowacji.</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Wstępną wycenę dokonujemy poprzez:
+          <ul>
+            <li>oględziny danego mebla,</li>
+            <li>online, poprzez wysłanie zdjęć na adres mailowy</li>
+            <li>telefonicznie, m.in. przy użyciu wiadomości MMS</li>
+          </ul>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn more React
-        </a>
-      </header>
+      </div>
+
+      <div className="Contact">
+
+      </div>
+
+
+      <div className="Footer">
+        <div className="FooterLeft">
+          <p>strona główna</p>
+          <p>galeria</p>
+          <p>cennik</p>
+          <p>kontakt</p>
+        </div>
+        <div className="FooterRight">
+          <p>Antyki Renowacja Mebli Zabytkowych</p>
+          <p>ul. Parkowa 5A, 42-286 Koszęcin</p>
+          <p>509 308 977</p>
+          <p>665 578 013</p>
+        </div>
+      </div>
     </div>
   );
 }
